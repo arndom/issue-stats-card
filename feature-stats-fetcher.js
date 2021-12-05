5d76869eb3da8824b7e8a4669edc7c1bb7fe98b8
arndom <aminonimisi2@gmail.com>
2021-12-06 00:13:16 +0100

    base logging test 02

--- feature-stats-fetcher.js
+++ feature-stats-fetcher.js
@@ -44,16 +44,12 @@ async function fetchIssueStats() {
 
   if (res.data.errors) {
     console.error(res.data.errors);
-    // throw Error(res.data.errors[0].message || "Could not fetch user");
   }
 
   console.log("all closed issues:",res.data.data.repository.all_time_closedIssues.totalCount)
   console.log("all open issues:",res.data.data.repository.all_time_openIssues.totalCount)
 
-  // console.log("response rate base array ",res.data.data.repository.response_rate.nodes)
   let base =  res.data.data.repository.response_rate.nodes
-
-  console.log(base.length)
   console.log("array of when issue were created:",base.map((issue) => issue.createdAt))
   console.log("array of when issue was first responded to:",base.map((issue) => issue.comments.nodes[0] !== undefined ? issue.comments.nodes[0].createdAt : "no comment reply"))
 
